1fba54f55a7feb4ab0c141267f6c6530f641148f
JayAlite <jayp@aliteprojects.in>
2022-11-04 12:33:13 +0530

    Added Add Remove Functionality and drop also working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import { useState } from "react";
 import DragFromOutside from "./components/DragFromOutside";
+import ReactGridLayout from "./components/TestComponent";
 
 function App() {
-  const getLayout = JSON.parse(window.localStorage.getItem("layouts"));
-
-  const [layout, setLayout] = useState(
-    getLayout !== null && Object.keys(getLayout).length > 0
-      ? getLayout
-      : { lg: [], md: [], sm: [], xs: [], xxs: [] }
-  );
-
-  const onLayoutChange = (layout, allLayout) => {
-    setLayout(allLayout);
-  };
-
   return (
     <>
       <div>Hey I am making dynamic grid layout</div>
-      <p>{JSON.stringify(layout, 2)}</p>
-      <DragFromOutside
-        onLayoutChange={onLayoutChange}
-        layout={layout}
-        setLayout={setLayout}
-      />
+      <DragFromOutside />
+      {/* <ReactGridLayout /> */}
     </>
   );
 }
--- src/components/DragFromOutside/index.jsx
+++ src/components/DragFromOutside/index.jsx
@@ -7,37 +7,75 @@ import "./drag-from-outside.css";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function DragFromOutside(props) {
+  const [appLayout, setAppLayout] = useState(null);
+  const [layoutComponent, setLayoutComponent] = useState({});
   const [currentBreakPoint, setCurrentBreakPoint] = useState("lg");
 
-  const mirrorToLocalStorage = (localStorageKey, itemToBeStore) => {
-    window.localStorage.setItem(localStorageKey, JSON.stringify(itemToBeStore));
-  };
-
   const onAddItem = () => {
-    props.setLayout((prevValue) => {
-      return {
-        ...prevValue,
-        [currentBreakPoint]: prevValue[currentBreakPoint]?.concat({
-          i: (prevValue[currentBreakPoint]?.length + 1).toString(),
-          x: (prevValue[currentBreakPoint].length * 2) % 12,
-          y: Infinity,
-          w: 2,
-          h: 1,
-        }),
-      };
+    const identifier = appLayout?.[currentBreakPoint]?.length
+      ? appLayout?.[currentBreakPoint]?.length.toString()
+      : "0";
+    setAppLayout({
+      ...appLayout,
+      [currentBreakPoint]: appLayout?.[currentBreakPoint]
+        ? [
+            ...appLayout[currentBreakPoint],
+            {
+              i: identifier,
+              x: (appLayout[currentBreakPoint].length * 2) % 12,
+              y: Infinity,
+              w: 2,
+              h: 1,
+            },
+          ]
+        : [
+            {
+              i: identifier,
+              x: 0,
+              y: Infinity,
+              w: 2,
+              h: 1,
+            },
+          ],
+    });
+    setLayoutComponent({
+      ...layoutComponent,
+      [identifier]: {
+        label: `widget - ${Math.random()}`,
+        properties: { id: "xyz", type: "chart" },
+      },
     });
   };
 
   const onRemoveItem = (currentItem, index) => {
-    props.setLayout((prevValue) => {
-      const filterItems = prevValue[currentBreakPoint]?.filter(
-        (item) => item?.i !== currentItem?.i
-      );
-      return {
-        ...prevValue,
-        [currentBreakPoint]: filterItems,
-      };
+    const layoutObject = {};
+    //remove item === current item from app layout
+    //result -> 0,1,3,4
+    let currentLayout = appLayout[currentBreakPoint].filter(
+      (item) => item.i !== currentItem.i
+    );
+
+    // create new layout component data i.e. key-value pair based on new layout having indices reseted in order (eg 0,1,2,3)
+    currentLayout = currentLayout.map((item, index) => {
+      if (parseInt(item.i) === index + 1) {
+        layoutObject[index.toString()] =
+          layoutComponent[(index + 1).toString()];
+      } else {
+        layoutObject[index.toString()] = layoutComponent[index.toString()];
+      }
+
+      if (parseInt(item.i) === index + 1) {
+        item.i = index.toString();
+      }
+      return item;
     });
+
+    setAppLayout({
+      ...appLayout,
+      [currentBreakPoint]: currentLayout,
+    });
+
+    setLayoutComponent(layoutObject);
   };
 
   const onDragStart = (event) => {
@@ -45,60 +83,76 @@ function DragFromOutside(props) {
     event?.dataTransfer?.setData("text/plain", setWidgetID);
   };
 
-  const onLayoutChange = (currentLayout, allLayout) => {
-    // if (
-    //   !allLayout[currentBreakPoint].find((li) => li.i === "__dropping-elem__")
-    // ) {
-    //   props.onLayoutChange(currentLayout, allLayout);
-    //   mirrorToLocalStorage("layouts", allLayout);
-    // }
-
-    props.onLayoutChange(currentLayout, allLayout);
-    mirrorToLocalStorage("layouts", allLayout);
-  };
-
   const onBreakpointChange = (newBreakpoint) => {
     setCurrentBreakPoint(newBreakpoint);
   };
 
   const onDrop = (layout, currentItem, event) => {
-    alert(JSON.stringify(currentItem, 2));
-    // const getWidgetID = event?.dataTransfer?.getData("text/plain");
-    // const tempArray = [];
-    // const allLayout = { lg: [], md: [], sm: [], xs: [], xxs: [] };
+    const getWidgetID = event?.dataTransfer?.getData("text/plain");
 
-    // tempArray.push({
-    //   i: getWidgetID,
-    //   x: currentItem?.x,
-    //   y: currentItem?.y,
-    //   w: currentItem?.w,
-    //   h: currentItem?.h,
-    // });
+    const identifier = layout?.[currentBreakPoint]?.length
+      ? layout?.[currentBreakPoint]?.length.toString()
+      : "0";
 
-    // allLayout[currentBreakPoint].push({
-    //   i: getWidgetID,
-    //   x: currentItem?.x,
-    //   y: currentItem?.y,
-    //   w: currentItem?.w,
-    //   h: currentItem?.h,
-    // });
+    setAppLayout({
+      ...appLayout,
+      [currentBreakPoint]: appLayout?.[currentBreakPoint]
+        ? [
+            ...appLayout[currentBreakPoint],
+            {
+              i: identifier,
+              x: currentItem?.x,
+              y: currentItem?.y,
+              w: currentItem?.w,
+              h: currentItem?.h,
+            },
+          ]
+        : [
+            {
+              i: identifier,
+              x: currentItem?.x,
+              y: currentItem?.y,
+              w: currentItem?.w,
+              h: currentItem?.h,
+            },
+          ],
+    });
 
-    // console.log('tempArray, allLayout', {tempArray, allLayout})
+    setLayoutComponent({
+      ...layoutComponent,
+      [identifier]: {
+        label: `widget - ${Math.random()}`,
+        properties: { id: getWidgetID, type: "chart" },
+      },
+    });
+  };
 
-    // onLayoutChange(tempArray, allLayout);
+  const onDragStop = (
+    layout,
+    oldItem,
+    newItem,
+    placeHolder,
+    event,
+    element
+  ) => {
+    setAppLayout({
+      ...appLayout,
+      [currentBreakPoint]: layout?.map((item) => item),
+    });
+  };
 
-    // props.setLayout((prevValue) => {
-    //   return {
-    //     ...prevValue,
-    //     [currentBreakPoint]: prevValue[currentBreakPoint]?.concat({
-    //       i: getWidgetID,
-    //       x: currentItem.x,
-    //       y: currentItem.y,
-    //       w: currentItem.w,
-    //       h: currentItem.h,
-    //     }),
-    //   };
-    // });
+  const onResizeStop = (
+    layout,
+    oldItem,
+    newItem,
+    placeHolder,
+    event,
+    element
+  ) => {
+    setAppLayout({
+      ...appLayout,
+      [currentBreakPoint]: layout?.map((item) => item),
+    });
   };
 
   return (
@@ -106,34 +160,46 @@ function DragFromOutside(props) {
       <div
         className="droppable-element"
         draggable
+        unselectable="on"
         data-widget-id="google-analytics-total-user-widget"
         onDragStart={onDragStart}
       ></div>
+
+      <div
+        className="droppable-element"
+        draggable
+        unselectable="on"
+        data-widget-id="google-analytics-total-widget"
+        onDragStart={onDragStart}
+      ></div>
+
       <button onClick={onAddItem}>Add Items</button>
       <ResponsiveGridLayout
-        className="layout responsive-grid-layout"
-        layouts={props?.layout}
+        // onLayoutChange={onLayoutChange}
+        onBreakpointChange={onBreakpointChange}
+        layout={appLayout}
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-        onLayoutChange={onLayoutChange}
-        onBreakpointChange={onBreakpointChange}
-        isDroppable
         useCSSTransforms
+        isDroppable
+        className="layout responsive-grid-layout"
         onDrop={onDrop}
-        onDropDragOver={(event) => {
-          event.preventDefault();
-        }}
+        onDragStop={onDragStop}
+        onResizeStop={onResizeStop}
       >
-        {props?.layout[currentBreakPoint]?.map((item, itemIndex) => {
-          return (
-            <div key={itemIndex}>
-              <span className="text">{item?.i}</span>
-              <button onClick={() => onRemoveItem(item, itemIndex)}>
-                Remove Item
+        {appLayout?.[currentBreakPoint]?.map(
+          (childElement, childElementIndex) => (
+            <div className="react-grid-item" key={childElementIndex}>
+              <button
+                className="deleteButton"
+                onClick={() => onRemoveItem(childElement, childElementIndex)}
+              >
+                Remove
               </button>
+              <div>{layoutComponent[childElement?.i]?.label}</div>
             </div>
-          );
-        })}
+          )
+        )}
       </ResponsiveGridLayout>
     </>
   );
